c772a01afdbceed827fdff1f0d1fde4cb2ffc809
doris <qianyh0806@163.com>
2018-04-11 23:18:40 +0800

    对manufacture配置完善

--- data/configure.py
+++ data/configure.py
@@ -4,9 +4,9 @@ import os
 
 # http://10.0.190.103:8080/JNMP20/services/BMPSystemService?wsdl
 conf = {
-	'url': 'http://172.17.1.207:8080/SOC2.0/services/BMPSystemService?wsdl',
+	'url': 'http://127.0.0.1:8080/SOC2.0/services/BMPSystemService?wsdl',
 	'db': {
-		'host': '172.17.1.213',
+		'host': '192.168.0.120',
 		'port': 3306,
 		'user': 'root',
 		'password': 'Anchiva@123',
@@ -20,13 +20,11 @@ conf = {
 
 manufacture = {
 	'origin': 'Manufacturers.xml',
-	'if_tag': 'Manufacturer',
-	'result_tag': 'Record/MAN_ID',
 	'tablename': 'bmp_manufacturers',
-	'manid': 'MAN_ID',
-	'where': {'MAN_NAME': '测试添加test_add'},
-	'class2class': 'bmp_class2class',
-	'attribtable': 'bmp_attribclass',
+	'field_manid': 'MAN_ID',
+	'query_fuzzy': {'MAN_NAME': 'like 信'},
+	'query_null': {'MAN_NAME': '测试查询结果为空'},
+	'query_exact': {'MAN_NAME': '测试添加test_add'},
 	'where_parent': {'parent_id': '11432'}
 }
 
