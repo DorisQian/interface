7d8430092b012af1527b42f62738baa397b3e611
DorisQian <qianyh@163.com>
2018-04-12 17:08:09 +0800

    解决console打印日志2次问题和实例化多次问题

--- common/Logger.py
+++ common/Logger.py
@@ -7,40 +7,36 @@ import os
 from logging import handlers
 
 
-class Logger:
+def log(name):
+    U"""
+    封装logger
+    :return:logging,供外部使用
     """
-    output logs to console and log files
-    """
-    def __init__(self):
-        U"""
-        封装log类
-        :return:
-        """
-        self.logging = logging
-        self.logger = logging.getLogger()
-        self.logger.setLevel('INFO')
+    logger = logging.getLogger(name)
+    logger.setLevel('INFO')
 
-        path = os.getcwd().split('interface')[0] + 'interface' + os.sep + 'logs' + os.sep
-        date = time.strftime('%Y-%m-%d')
-        file = path + date + '.log'
+    path = os.getcwd().split('interface')[0] + 'interface' + os.sep + 'logs' + os.sep
+    date = time.strftime('%Y-%m-%d')
+    file = path + date + '.log'
 
-        ch = logging.StreamHandler()
-        # ch.setLevel(self.level)
+    ch = logging.StreamHandler()
+    # ch.setLevel(self.level)
 
-        # save five days log(backupcount)
-        fh = handlers.TimedRotatingFileHandler(file, when='D', interval=1, backupCount=5, encoding='utf8')
-        # fh.setLevel(self.level)
-        fh.suffix = '%Y-%m-%d.log'
+    # save five days log(backupcount)
+    fh = handlers.TimedRotatingFileHandler(file, when='D', interval=1, backupCount=5, encoding='utf8')
+    # fh.setLevel(self.level)
+    fh.suffix = '%Y-%m-%d.log'
 
-        log_format = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)d]  %(message)s')
-        ch.setFormatter(log_format)
-        fh.setFormatter(log_format)
+    log_format = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)d]  %(message)s')
+    ch.setFormatter(log_format)
+    fh.setFormatter(log_format)
 
-        self.logger.addHandler(ch)
-        self.logger.addHandler(fh)
-
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
 
 if __name__ == '__main__':
-    logger = Logger()
-    logging.info('testing')
-    logging.error('test111')
+
+    logging = log('test')
+    logging.info('test')
+
